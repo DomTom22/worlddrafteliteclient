af9c4c7ae14af12974467884368b3148afa0c6ef
scoopapa <pccompositor@aim.com>
2020-12-24 13:40:37 -0500

    Revert "Lol"
    This reverts commit 4bac56e9bd8967e30ab58f3d1052e2da4a504f55.

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'dragonheaven',
+	id: 'showdown',
 	host: 'scoopapa-dh.glitch.me',
 	port: 443,
 	httpport: 8000,
