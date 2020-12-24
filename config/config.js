4bac56e9bd8967e30ab58f3d1052e2da4a504f55
scoopapa <pccompositor@aim.com>
2020-12-24 12:59:19 -0500

    Lol

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'showdown',
+	id: 'dragonheaven',
 	host: 'scoopapa-dh.glitch.me',
 	port: 443,
 	httpport: 8000,
