3a2ec176a82a63eacbe0b633baec5b8e791ea676
scoopapa <pccompositor@aim.com>
2021-01-04 00:26:20 -0500

    rebuild after pulling changes

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (1acc420e/55fadf43)";
+Config.version = "0.11.2 (1b8b3ba2/55fadf43)";
 
 Config.routes = {
 	root: 'scoopapa-dh.glitch.me',
