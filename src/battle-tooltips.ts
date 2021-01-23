c27693d65823066f9e9d2e0ff4ec69e244ecc1cb
scoopapa <pccompositor@aim.com>
2021-01-23 16:37:34 -0500

    add log messages to investigate battle tooltip issues

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -256,6 +256,7 @@ class BattleTooltips {
 	}
 
 	showTooltip(elem: HTMLElement) {
+		console.log('showTooltip');
 		const args = (elem.dataset.tooltip || '').split('|');
 		const [type] = args;
 		/**
@@ -726,6 +727,7 @@ class BattleTooltips {
 	showPokemonTooltip(
 		clientPokemon: Pokemon | null, serverPokemon?: ServerPokemon | null, isActive?: boolean, illusionIndex?: number
 	) {
+		console.log('showPokemonTooltip');
 		const pokemon = clientPokemon || serverPokemon!;
 		let text = '';
 		let genderBuf = '';
