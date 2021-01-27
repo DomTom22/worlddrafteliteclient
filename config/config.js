b896c07f288bbda45d4d9de5051b5c92eff05331
scoopapa <pccompositor@aim.com>
2021-01-26 21:41:49 -0500

    trying to fix replays

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (23a4a259/55fadf43)";
+Config.version = "0.11.2 (34a9686f/55fadf43)";
 
 Config.routes = {
 	root: 'scoopapa-dh.glitch.me',
