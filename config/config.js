a7d999cbce839e217b347f834e9011aed622d2a2
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2021-05-26 08:12:04 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (fff3e66d/55fadf43)";
+Config.version = "0.11.2 (e39deacb/55fadf43)";
 
 Config.routes = {
 	root: 'worlddraftelite.glitch.me',
