17006c783430b4c265ed1c8a583e0a8331b19ba1
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2021-06-04 10:56:57 -0500

    galar mega update

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (56c7a213/55fadf43)";
+Config.version = "0.11.2 (f2c3276a/55fadf43)";
 
 Config.routes = {
 	root: 'worlddraftelite.glitch.me',
