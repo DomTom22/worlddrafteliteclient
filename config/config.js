a9e95241db8e9e611890d636e4a4b04ee525b3ac
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2021-06-11 12:40:57 -0500

    nat dex fix

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (1cbc2462/55fadf43)";
+Config.version = "0.11.2 (9241b713/55fadf43)";
 
 Config.routes = {
 	root: 'worlddraftelite.glitch.me',
