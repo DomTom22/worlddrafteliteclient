b3b888fb308cff21edbc3d0a42bb07a72d46a12d
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2021-07-21 13:41:20 -0500

    Uranium Tier Addition

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (7b2d13c7/55fadf43)";
+Config.version = "0.11.2 (8732b263/55fadf43)";
 
 Config.routes = {
 	root: 'fantasyshowdown.herokuapp.com',
