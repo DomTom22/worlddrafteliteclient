8346c0632006512f2febfd45b77f49352718e7f5
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2022-05-26 21:28:23 -0500

    Revert "Revert "update""
    This reverts commit 6c4f554d2908c60a70db1bb0144e68556d9c7de2.

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (63615fce/55fadf43)";
+Config.version = "0.11.2 (5c1c81ad/55fadf43)";
 
 Config.routes = {
 	root: 'fantasyshowdown.herokuapp.com',
