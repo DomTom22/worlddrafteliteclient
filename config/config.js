a4104196927add1ea23ac11f9837dd60d7519253
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2022-05-26 21:39:39 -0500

    update

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: '162.24.128.159',
+	host: 'sim3.psim.us',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
