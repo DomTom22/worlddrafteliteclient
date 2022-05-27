7d411732acbc578d4cf15ddfa7a8f71a62caf180
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2022-05-26 21:28:08 -0500

    Revert "updfate"
    This reverts commit d982644b8c044585c6da937765bd2b3290d4eaa6.

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (edfe6b81/55fadf43)";
+Config.version = "0.11.2 (41b880a8/55fadf43)";
 
 Config.routes = {
 	root: 'fantasyshowdown.herokuapp.com',
