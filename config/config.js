e8064b0bf0f0d3978cf160dcf08924c71aeb107c
DomTom22 <54041966+DomTom22@users.noreply.github.com>
2022-11-20 09:27:24 -0600

    update

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (4360df3b/55fadf43)";
+Config.version = "0.11.2 (28bfe368/55fadf43)";
 
 Config.routes = {
 	root: 'fantasyshowdown.herokuapp.com',
